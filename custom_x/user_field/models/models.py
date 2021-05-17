68ed527cb7afdfccc08315edc25ab24a991ade34
kanakimayadzaki <kanakimayadzaki@gmail.com>
2021-05-18 00:09:02 +0300

    Replace field author into selection

--- custom_x/user_field/models/models.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class TaskUserField(models.Model):
-    _inherit = 'task_estimation.task_estimation'
-
-    author = fields.Many2one('res.partner', string='Author')
-
