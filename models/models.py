6e559f9170c4456f84d8c0574f4e04215304f7ef
kanakimayadzaki <kanakimayadzaki@gmail.com>
2021-07-04 09:06:51 +0300

    Total redesign

--- models/models.py
+++ models/models.py
@@ -20,7 +20,7 @@ class estimator(models.Model):
         ('1.7', '1.7'),
     ], string="Comprehension index", default='1.2')
     hours_perfect = fields.Float(store=True, compute="total_task_calc", string="Perfect (hours)")
-    hours_real_time = fields.Float(store=True, compute="calc_peal_time", string="Peal Time (hours)")
+    hours_real_time = fields.Float(store=True, compute="calc_peal_time", string="Real Time (hours)")
     hours_low_performance = fields.Float(store=True, compute="calc_ow_performance", string="Low performance (hours)")
     notes = fields.Text(string="Description")
     unit_works_lines = fields.One2many('task_estimation.lines', 'task_id', string="Tasks Lines")
@@ -28,9 +28,22 @@ class estimator(models.Model):
                                 index=True,
                                 default=lambda self: _('New'))
     units_id = fields.Many2one('task_estimation.work_units', string='Units ID')
-    author = fields.Many2one('hr.employee', string="Author")
+    author = fields.Many2one('estimator.command', string="Author")
     total_task_time = fields.Float(store=True, compute="total_task_calc", string="Total Time (hh:mm)")
     tasks_count = fields.Integer(string='Count of author tasks', compute='get_count_tasks')
+    task_id = fields.Many2one('estimator.project')
+    role = fields.Selection([
+        ('analyst', 'Analyst'),
+        ('designer', 'Designer'),
+        ('develop', 'Developer'),
+        ('qa', 'QA'),
+        ('PM', 'PM')
+    ], string='Role')
+
+    @api.onchange('author')
+    def _onchange_author(self):
+        write_data = self.env['estimator.command'].search([('id', '=', self.author.id)])
+        self.role = write_data.role
 
     @api.depends('unit_works_lines.total_time')
     def total_task_calc(self):
@@ -72,6 +85,7 @@ class estimator(models.Model):
         count = self.env['estimator.task_estimation'].search_count([('author.id', '=', self.author.id)])
         self.tasks_count = count
 
+
 class TaskEstimationLines(models.Model):
     _name = 'task_estimation.lines'
     _description = 'Task Lines'
@@ -86,3 +100,59 @@ class TaskEstimationLines(models.Model):
     def total_calc(self):
         for record in self:
             record.total_time = record.workunit_id.minutes_to_do * record.workunit_quantity
+
+
+class Command(models.Model):
+    _name = 'estimator.command'
+    _description = 'Command'
+
+    name = fields.Many2one('hr.employee', string="Author")
+    role = fields.Selection([
+        ('analyst', 'Analyst'),
+        ('designer', 'Designer'),
+        ('develop', 'Developer'),
+        ('qa', 'QA'),
+        ('PM', 'PM')
+    ], string='Role')
+
+
+class Project(models.Model):
+    _name = 'estimator.project'
+    _description = 'Project'
+
+    name = fields.Char(string="Project name", required=True)
+    company = fields.Char(string="Company name", required=True)
+    total_perfect_hours = fields.Float(string="Total Perfect Hours", compute='total_calc_per_hours')
+    total_real_time = fields.Float(string="Total Real Time", compute='total_calc_real_time')
+    total_low_performance = fields.Float(string="Total Low Performance", compute='total_calc_low_performance')
+    tasks = fields.One2many('estimator.task_estimation', 'task_id', string="List of Tasks")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirm'),
+        ('done', 'Done'),
+        ('cancelled', 'Cancelled')
+        ], default='draft', string="Status")
+
+    @api.depends('tasks.hours_perfect')
+    def total_calc_per_hours(self):
+        for task in self:
+            total = 0.0
+            for line in task.tasks:
+                total += line.hours_perfect
+            task.total_perfect_hours = total
+
+    @api.depends('tasks.hours_real_time')
+    def total_calc_real_time(self):
+        for task in self:
+            total = 0.0
+            for line in task.tasks:
+                total += line.hours_real_time
+            task.total_real_time = total
+
+    @api.depends('tasks.hours_low_performance')
+    def total_calc_low_performance(self):
+        for task in self:
+            total = 0.0
+            for line in task.tasks:
+                total += line.hours_low_performance
+            task.total_low_performance = total
